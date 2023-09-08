068e4627072fd7c584c33615b0a95098c3617019
Agung Wicaksono <agungwck99@gmail.com>
2023-09-09 06:45:23 +0700

    feat: add prettier

--- src/components/Ranking.svelte
+++ src/components/Ranking.svelte
@@ -1,50 +1,50 @@
 <script>
-  export let rankings = {};
-  
-  let rankingKeys = [];
+    export let rankings = {}
 
-  $: {
-    rankingKeys = Object.keys(rankings);
-    console.log("Updated rankingKeys:", rankingKeys);
-  }
-  
-  function getTopFour(arr) {
-    return arr.slice(0, 4);
-  }
+    let rankingKeys = []
+
+    $: {
+        rankingKeys = Object.keys(rankings)
+        console.log('Updated rankingKeys:', rankingKeys)
+    }
+
+    function getTopFour(arr) {
+        return arr.slice(0, 4)
+    }
 </script>
 
-<style>
-  th, td {
-    padding: 10px;
-  }
-
-  .rank {
-    text-align: center;
-  }
-
-</style>
-
 <div>
-  <h2>Ranking</h2>
-  {#each rankingKeys as key}
-    <h3>{key}</h3>
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Juara</th>
-          <th>Nama</th>
-          <th>Asal</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each getTopFour(rankings[key]) as item}
-          <tr>
-            <td class="rank">{item.rank}</td>
-            <td>{item.fighter.fullName}</td>
-            <td>{item.fighter.teamName}</td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  {/each}
+    <h2>Ranking</h2>
+    {#each rankingKeys as key}
+        <h3>{key}</h3>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Juara</th>
+                    <th>Nama</th>
+                    <th>Asal</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each getTopFour(rankings[key]) as item}
+                    <tr>
+                        <td class="rank">{item.rank}</td>
+                        <td>{item.fighter.fullName}</td>
+                        <td>{item.fighter.teamName}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {/each}
 </div>
+
+<style>
+    th,
+    td {
+        padding: 10px;
+    }
+
+    .rank {
+        text-align: center;
+    }
+</style>
--- src/components/Schedule.svelte
+++ src/components/Schedule.svelte
@@ -1,101 +1,88 @@
 <script>
-    export let fights = [];
-    
-    let selectedTeam = ''; // Variable to keep track of the selected team
-    let groupedFights = {};
-    let selectedDate = '';
+    import './schedule.css'
+
+    export let fights = []
+
+    let selectedTeam = '' // Variable to keep track of the selected team
+    let groupedFights = {}
+    let selectedDate = ''
 
     $: {
         groupedFights = fights.reduce((acc, fight) => {
-            const date = fight.sessionStartDate;
+            const date = fight.sessionStartDate
             if (!acc[date]) {
-                acc[date] = [];
+                acc[date] = []
             }
-            acc[date].push(fight);
-            return acc;
-        }, {});
-        selectedDate = Object.keys(groupedFights)[0] || '';
+            acc[date].push(fight)
+            return acc
+        }, {})
+        selectedDate = Object.keys(groupedFights)[0] || ''
     }
 
     function capitalizeFirstLetter(word) {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
     }
 
     function formatName(name) {
-        const words = name.split(' ');
-        const capitalizedWords = words.map(capitalizeFirstLetter);
-        return capitalizedWords.join(' ');
+        const words = name.split(' ')
+        const capitalizedWords = words.map((word, index) => {
+            if (index > 1) {
+                return word.charAt(0).toUpperCase() + '.'
+            }
+            return capitalizeFirstLetter(word)
+        })
+        return capitalizedWords.join(' ')
     }
 
     function selectTeam(team) {
-        selectedTeam = team;
+        selectedTeam = team
     }
 </script>
-<style>
-    .fight-container {
-        display: grid;
-        grid-template-columns: 1fr 5fr 1fr 5fr;
-        align-items: center;
-        padding: 10px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
-    .completed {
-      background-color: rgb(223, 223, 223);
-    }
-    .highlight {
-      background-color: yellow;
-    }
-    .atlethe {
-        display: grid;
-        grid-template-columns: 1fr 3fr;
-    }
-    .number {
-        padding: 0 auto;
-    }
-    .fighter {
-        font-weight: bold;
-    }
-    .team {
-        font-style: italic;
-    }
-    .vs {
-        text-align: center;
-        padding: 0 auto;
-    }
-    .fight-wrapper {
-        padding-bottom: 120px; /* Adjust this value as needed */
-    }
-    </style>
-    
+
 <!-- Date Tabs -->
 <div>
     {#each Object.keys(groupedFights) as date}
-        <button on:click={() => selectedDate = date}>{date}</button>
+        <button on:click={() => (selectedDate = date)}>{date}</button>
     {/each}
 </div>
 
 <!-- Team Filter -->
 <div>
-  <input type="text" bind:value={selectedTeam} placeholder="Filter by team name" />
+    <input type="text" bind:value={selectedTeam} placeholder="Filter by team name" />
 </div>
 
 <!-- Fights -->
 <div class="fight-wrapper">
     {#if selectedDate && groupedFights[selectedDate]}
-    {#each groupedFights[selectedDate] as fightData}
-    <div class="fight-container" class:completed={fightData.isCompleted} class:highlight={fightData.team1AlternateName === selectedTeam || fightData.team2AlternateName === selectedTeam}>
-        <div class="number">{fightData.fightNumber}</div>
-        <div class="atlethe">
-            <span class="team" on:click={() => selectTeam(fightData.team1AlternateName)}>({fightData.team1AlternateName})</span>
-            <span class="fighter">{formatName(fightData.fighter1FullName)}</span>
-        </div>
-        <div class="vs">VS</div>
-        <div class="atlethe">
-            <span class="team" on:click={() => selectTeam(fightData.team2AlternateName)}>({fightData.team2AlternateName})</span>
-            <span class="fighter">{formatName(fightData.fighter2FullName)}</span>
-        </div>
-    </div>
-    {/each}
+        {#each groupedFights[selectedDate] as fightData}
+            <div
+                class="fight-container"
+                class:stroke={fightData.fighter1FullName == '' || fightData.fighter2FullName == ''}
+                class:completed={fightData.isCompleted ||
+                    fightData.fighter1FullName == '' ||
+                    fightData.fighter2FullName == ''}
+                class:highlight={(fightData.fighter1FullName != '' && fightData.team1AlternateName === selectedTeam) ||
+                    (fightData.fighter2FullName != '' && fightData.team2AlternateName === selectedTeam)}
+            >
+                <div class="number">{fightData.fightNumber}</div>
+                <div class="atlethe">
+                    <span class="team" on:click={() => selectTeam(fightData.team1AlternateName)}
+                        >({fightData.team1AlternateName})</span
+                    >
+                    <span class="fighter {fightData.winnerFighter !== fightData.fighter1Id ? 'stroke' : ''}"
+                        >{formatName(fightData.fighter1FullName)}</span
+                    >
+                </div>
+                <div class="vs">VS</div>
+                <div class="atlethe">
+                    <span class="team" on:click={() => selectTeam(fightData.team2AlternateName)}
+                        >({fightData.team2AlternateName})</span
+                    >
+                    <span class="fighter {fightData.winnerFighter !== fightData.fighter2Id ? 'stroke' : ''}"
+                        >{formatName(fightData.fighter2FullName)}</span
+                    >
+                </div>
+            </div>
+        {/each}
     {/if}
 </div>
