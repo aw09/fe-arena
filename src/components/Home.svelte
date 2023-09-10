0d05cba5e9a370617b14e93e9e4d819c67a6b1e4
Agung Wicaksono <agungwck99@gmail.com>
2023-09-11 00:52:34 +0700

    feat: add versus page

---
+++ src/components/Home.svelte
@@ -0,0 +1,144 @@
+<script>
+    import { onMount, tick } from 'svelte'
+    import { db } from '../firebase.js'
+    import { ref, onValue, off } from 'firebase/database'
+    import ScheduleComponent from './Schedule.svelte'
+    import Ranking from './Ranking.svelte'
+
+    let fights = []
+    let rankings = {}
+    let selectedTab = 'fight' // Default selected tab
+    let lastUpdated = null
+    let lastUpdatedString = ''
+    let intervalId
+
+    $: {
+        if (lastUpdated) {
+            updateLastUpdatedString()
+        }
+    }
+    function updateLastUpdatedString() {
+        const now = new Date().getTime()
+        const difference = Math.floor((now - lastUpdated) / 1000) // in seconds
+
+        if (difference < 60) {
+            lastUpdatedString = `${difference} seconds ago`
+        } else if (difference < 3600) {
+            lastUpdatedString = `${Math.floor(difference / 60)} minutes ago`
+        } else {
+            lastUpdatedString = `${Math.floor(difference / 3600)} hours ago`
+        }
+    }
+
+    onMount(() => {
+        const fightsRef = ref(db, 'fightsCompressed')
+        const rankingsRef = ref(db, 'ranking')
+        const lastUpdatedRef = ref(db, 'lastUpdated')
+
+        // Listen for changes in the fights database
+        const unsubscribeFights = onValue(
+            fightsRef,
+            snapshot => {
+                if (snapshot.exists()) {
+                    fights = [...snapshot.val()]
+                    console.log(fights)
+                } else {
+                    console.log('No data available')
+                }
+            },
+            error => {
+                console.error(error)
+            }
+        )
+
+        // Listen for changes in the rankings database
+        const unsubscribeRankings = onValue(
+            rankingsRef,
+            snapshot => {
+                if (snapshot.exists()) {
+                    console.log(snapshot.val())
+                    rankings = snapshot.val()
+                    console.log(rankings)
+                } else {
+                    console.log('No ranking data available')
+                }
+            },
+            error => {
+                console.error(error)
+            }
+        )
+
+        // Listen for changes to the lastUpdated timestamp
+        const unsubscribeLastUpdated = onValue(
+            lastUpdatedRef,
+            snapshot => {
+                if (snapshot.exists()) {
+                    lastUpdated = snapshot.val() * 1000
+                } else {
+                    console.log('No lastUpdated data available')
+                }
+            },
+            error => {
+                console.error(error)
+            }
+        )
+
+        intervalId = setInterval(() => {
+            updateLastUpdatedString()
+            tick()
+        }, 10000)
+
+        return () => {
+            off(fightsRef, 'value', unsubscribeFights)
+            off(rankingsRef, 'value', unsubscribeRankings)
+            off(lastUpdatedRef, 'value', unsubscribeLastUpdated)
+            clearInterval(intervalId)
+        }
+    })
+</script>
+
+<div class="last-updated">
+    {#if lastUpdatedString}
+        <p>Last updated: {lastUpdatedString}</p>
+    {/if}
+</div>
+
+<!-- Floating Tab buttons -->
+<div class="tab-buttons">
+    <button on:click={() => (selectedTab = 'fight')}>Fights</button>
+    <button on:click={() => (selectedTab = 'ranking')}>Ranking</button>
+</div>
+
+<!-- Tab content -->
+{#if selectedTab === 'fight'}
+    <ScheduleComponent {fights} />
+{:else if selectedTab === 'ranking'}
+    <Ranking {rankings} />
+{/if}
+
+<style>
+    .tab-buttons {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background-color: #fff;
+        display: flex;
+        height: fit-content;
+    }
+
+    .tab-buttons > * {
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .last-updated {
+        position: fixed;
+        bottom: 40px;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        background-color: rgba(255, 255, 255, 0.8);
+        padding: 5px;
+    }
+</style>
---
+++ src/components/Versus.svelte
@@ -0,0 +1,70 @@
+<div class="background">
+    <div class="fighter-container red">
+        <img src="../images/1.png" alt="Fighter Red" class="fighter red" />
+        <img src="../images/bg-name-red.png" alt="bg-name-red" class="bg-name" />
+        <p class="name red">WRESTLER1</p>
+    </div>
+    <div class="fighter-container blue">
+        <img src="../images/2.png" alt="Fighter Blue" class="fighter blue" />
+        <img src="../images/bg-name-blue.png" alt="bg-name-blue" class="bg-name" />
+        <p class="name blue">WRESTLER2</p>
+    </div>
+</div>
+
+<style>
+    .background {
+        position: relative;
+        background-image: url('../images/bg-versus.png');
+        background-size: cover;
+        background-position: center;
+        height: 100vh;
+        z-index: 1;
+    }
+    @keyframes enterFromBottom {
+        from {
+            bottom: -50%;
+        }
+        to {
+            bottom: 5%;
+        }
+    }
+    .fighter-container {
+        width: 550px;
+        position: absolute;
+        bottom: 5%;
+        z-index: 2;
+        animation: enterFromBottom 1s ease-out;
+    }
+    .bg-name {
+        position: absolute;
+        bottom: 0;
+        z-index: 3;
+        width: 100%;
+        left: 0;
+        right: 0;
+        color: white;
+    }
+    .fighter {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+
+    .red {
+        left: 0;
+    }
+
+    .blue {
+        right: 0;
+    }
+    .name {
+        margin-bottom: 20px;
+        padding: 0 30px;
+        position: absolute;
+        bottom: 0;
+        color: white;
+        z-index: 4;
+        font-size: 60px;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+</style>
