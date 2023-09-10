fe82154e00b72016199ae30e7e985e4bcd89f58d
Agung Wicaksono <agungwck99@gmail.com>
2023-09-11 06:01:05 +0700

    feat: update basepath and homepage

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,10 @@
     import { Router, Link, Route } from 'svelte-routing'
     import Home from './components/Home.svelte'
     import Versus from './components/Versus.svelte'
+    let basepath = window.location.hostname === 'aw09.github.io' ? '/fe-arena' : ''
 </script>
 
-<Router>
+<Router {basepath}>
     <Route path="/" component={Home} />
     <Route path="/versus-screen" component={Versus} />
 </Router>
