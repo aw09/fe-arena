a962695c5613243e35002eedbe9d4299b99b612e
Agung Wicaksono <agungwck99@gmail.com>
2023-09-12 17:10:20 +0700

    feat: update util

--- src/components/Versus.svelte
+++ src/components/Versus.svelte
@@ -1,14 +1,80 @@
+<script>
+    import { onMount } from 'svelte'
+    import { db } from '../firebase.js'
+    import { ref, onValue, off } from 'firebase/database'
+    import { formatName, pathImageFighter } from '../util.js'
+
+    let fights = []
+    let selectedFight = null
+
+    function restartAnimation(element) {
+        element.classList.remove('animate')
+        void element.offsetWidth
+        element.classList.add('animate')
+    }
+
+    onMount(() => {
+        const fightsRef = ref(db, 'fightsCompressed')
+
+        const unsubscribeFights = onValue(
+            fightsRef,
+            snapshot => {
+                if (snapshot.exists()) {
+                    fights = [...snapshot.val()]
+                    const newSelectedFight = fights.find(fight => fight.isReady && !fight.isCompleted)
+
+                    // Check if selectedFight has changed
+                    if (JSON.stringify(newSelectedFight) !== JSON.stringify(selectedFight)) {
+                        selectedFight = newSelectedFight
+
+                        // Trigger the animation again
+                        const elements = document.querySelectorAll('.fighter-container')
+                        elements.forEach(restartAnimation)
+                    }
+                } else {
+                    console.log('No data available')
+                }
+            },
+            error => {
+                console.error(error)
+            }
+        )
+
+        return () => {
+            off(fightsRef, 'value', unsubscribeFights)
+        }
+    })
+</script>
+
 <div class="background">
-    <div class="fighter-container red">
-        <img src="../images/1.png" alt="Fighter Red" class="fighter red" />
-        <img src="../images/bg-name-red.png" alt="bg-name-red" class="bg-name" />
-        <p class="name red">WRESTLER1</p>
-    </div>
-    <div class="fighter-container blue">
-        <img src="../images/2.png" alt="Fighter Blue" class="fighter blue" />
-        <img src="../images/bg-name-blue.png" alt="bg-name-blue" class="bg-name" />
-        <p class="name blue">WRESTLER2</p>
-    </div>
+    {#if selectedFight}
+        <div class="fighter-container red animate">
+            <img
+                src={`../images/${pathImageFighter(
+                    selectedFight.fighter1FullName,
+                    selectedFight.team1AlternateName,
+                    selectedFight.weightCategoryAlternateName
+                )}`}
+                alt="Fighter Red"
+                class="fighter red"
+            />
+            <img src="../images/bg-name-red.png" alt="bg-name-red" class="bg-name" />
+            <p class="name red">{formatName(selectedFight.fighter1FullName, 2)}</p>
+        </div>
+        <div class="fighter-container blue animate">
+            <img
+                src={`../images/${pathImageFighter(
+                    selectedFight.fighter2FullName,
+                    selectedFight.team2AlternateName,
+                    selectedFight.weightCategoryAlternateName
+                )}`}
+                alt="Fighter Blue"
+                class="fighter blue"
+            />
+            <img src="../images/bg-name-blue.png" alt="bg-name-blue" class="bg-name" />
+            <p class="name blue">{formatName(selectedFight.fighter2FullName, 2)}</p>
+        </div>
+    {/if}
 </div>
 
 <style>
@@ -28,12 +94,14 @@
             bottom: 5%;
         }
     }
+    .animate {
+        animation: enterFromBottom 1s ease-out;
+    }
     .fighter-container {
-        width: 550px;
+        width: 700px;
         position: absolute;
         bottom: 5%;
         z-index: 2;
-        animation: enterFromBottom 1s ease-out;
     }
     .bg-name {
         position: absolute;
@@ -64,7 +132,7 @@
         bottom: 0;
         color: white;
         z-index: 4;
-        font-size: 60px;
+        font-size: 80px;
         font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     }
 </style>
---
+++ src/util.js
@@ -0,0 +1,31 @@
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+}
+
+export const formatName = (name, limit = null) => {
+    const words = name.split(' ')
+    const capitalizedWords = words.map((word, index) => {
+        if (index > 1) {
+            return word.charAt(0).toUpperCase() + '.'
+        }
+        return capitalizeFirstLetter(word)
+    })
+    if (limit) {
+        return capitalizedWords.slice(0, limit).join(' ')
+
+    }
+    else capitalizedWords.join(' ')
+}
+
+export const pathImageFighter = (fighter, team, weight) => {
+    const weightCategory = weight.replace(" - ", "").replace(" kg", "").replace("kg", "");
+
+    const fighterName = fighter
+        .replace('.', "")
+        .split(' ')
+        .slice(0, 2)
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join('_');
+
+    return `${weightCategory}/${fighterName}_${team}.png`;
+};
